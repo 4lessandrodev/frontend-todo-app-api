6351f9d250ca6afa98e62c44ac24eda4e4f976ff
Alessandro Menezes <alessandroadm@live.com>
2020-07-12 21:35:56 -0300

    Feat(app) Adding components

---
+++ src/main/App.jsx
@@ -0,0 +1,15 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+import Todo from '../todo/Todo';
+import About from './../about/About';
+import Menu from './../menu/Menu';
+
+export default props => (
+    <div className="container">
+        <Menu />
+        <Todo />
+        <About />
+    </div>
+)
---
+++ src/todo/Todo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1>Todo</h1>
+  </div>  
+);
